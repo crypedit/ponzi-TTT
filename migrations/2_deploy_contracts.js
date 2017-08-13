694a39db96a005c03f15275040f9e3bd41f47a52
Fan Jiang <fanjiang@thoughtworks.com>
2017-08-13 18:50:08 +0800

    update instructions for contract initialization

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,8 @@
 var PonziTTT = artifacts.require("./PonziTTT.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(PonziTTT, [], 4);
+  // Change following address list when deploy to production
+  // The deploy account will be owner by default
+  var owners = [];
+  deployer.deploy(PonziTTT, owners, 4);
 };
